ee2467b9746dc5dd6cfb4bc6c698af428e074e87
raz <razmagal@gmail.com>
2026-03-20 15:33:36 +0200

    Updated snowballs to rocks more unstaged files

---
+++ src/main/java/com/talhanation/recruits/entities/ThrownRockEntity.java
@@ -0,0 +1,48 @@
+package com.talhanation.recruits.entities;
+
+import com.talhanation.recruits.init.ModEntityTypes;
+import com.talhanation.recruits.init.ModItems;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
+
+public class ThrownRockEntity extends ThrowableItemProjectile {
+
+    public ThrownRockEntity(EntityType<? extends ThrowableItemProjectile> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    public ThrownRockEntity(Level level, LivingEntity shooter) {
+        super(ModEntityTypes.THROWN_ROCK.get(), shooter, level);
+    }
+
+    @Override
+    protected Item getDefaultItem() {
+        return ModItems.ROCK.get();
+    }
+
+    @Override
+    protected void onHit(HitResult result) {
+        super.onHit(result);
+
+        if (!this.level().isClientSide) {
+            ServerLevel serverLevel = (ServerLevel) this.level();
+
+            serverLevel.sendParticles(ParticleTypes.CRIT,
+                    this.getX(), this.getY(), this.getZ(),
+                    5, 0.1, 0.1, 0.1, 0.02);
+
+            serverLevel.playSound(null, this.getX(), this.getY(), this.getZ(),
+                    SoundEvents.STONE_HIT, SoundSource.NEUTRAL, 0.6F, 1.2F);
+
+            this.discard();
+        }
+    }
+}
---
+++ src/main/java/com/talhanation/recruits/items/RockItem.java
@@ -0,0 +1,10 @@
+package com.talhanation.recruits.items;
+
+import net.minecraft.world.item.Item;
+
+public class RockItem extends Item {
+
+    public RockItem(Properties properties) {
+        super(properties);
+    }
+}
